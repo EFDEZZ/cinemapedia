e2a9896c6aed3a73126adebb7cf38d91f333bead
EDRIEN FDEZ <edriennn2001@gmail.com>
2024-08-10 12:40:49 -0400

    custom appbar y slideshow implementado

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -33,17 +34,17 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    if(nowPlayingMovies.isEmpty) return const Center(child: CircularProgressIndicator());
+    // final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final slideShowMovies = ref.watch(moviesSlideShowProvider);
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
-      );
+    if(slideShowMovies.isEmpty) return const Center(child: CircularProgressIndicator(strokeWidth: 2,));
+
+    return Column(
+      children: [
+        
+        const CustomAppbar(),
+        MoviesSlideshow(movies: slideShowMovies)
+      ],
+    );
   }
 }
