d61b477d52553535d2f4cb129fd04750ce976a7e
EDRIEN FDEZ <edriennn2001@gmail.com>
2024-08-20 21:38:24 -0400

    Creando los providers para mostrar la pelicula y guardarla en un cache local

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends StatefulWidget {
   final String movieID;
   const MovieScreen({super.key, required this.movieID});
+
+  @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MovieID: $movieID'),
+        title: Text('MovieID: ${widget.movieID}'),
       ),
     );
   }
