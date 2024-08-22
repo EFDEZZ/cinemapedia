fbac234017ce0ac85d203ef98b90ed076d72fd35
EDRIEN FDEZ <edriennn2001@gmail.com>
2024-08-21 23:06:27 -0400

    bases para Vista de detlles de peliculpa implemantadas

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,7 +1,10 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
+import 'package:cinemapedia/presentation/widgets/shared/poster_gradient.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../providers/providers.dart';
 
 class MovieScreen extends ConsumerStatefulWidget {
   final String movieID;
@@ -32,9 +35,52 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     }
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MovieID: ${widget.movieID}'),
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [
+          _CustomSliver(movie: movie,),
+        ],
+
       ),
+    );
+  }
+}
+
+class _CustomSliver extends StatelessWidget {
+  final Movie movie;
+  const _CustomSliver({
+    required this.movie
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height*0.7,
+      foregroundColor: Colors.white,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        title: Text(
+          movie.title,
+          style: const TextStyle(fontSize: 20, color: Colors.white),
+          textAlign: TextAlign.start,
+          
+        ),
+        background: Stack(
+          children: [
+            SizedBox.expand(
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                ),
+            ),
+            const PosterGradient(),
+            const BackArrowGradient(),
+          ],
+        ),
+      ),
+
     );
   }
 }
--- lib/presentation/widgets/shared/poster_gradient.dart
+++ lib/presentation/widgets/shared/poster_gradient.dart
@@ -21,3 +21,23 @@ class PosterGradient extends StatelessWidget {
     ));
   }
 }
+class BackArrowGradient extends StatelessWidget {
+  const BackArrowGradient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Positioned.fill(
+        child: DecoratedBox(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              stops: [
+            0.0, 0.3
+          ],
+              colors: [
+            Colors.black87,
+            Colors.transparent,
+          ])),
+    ));
+  }
+}
