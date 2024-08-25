73578f02b91748104c311848ca69818a4f547936
EDRIEN FDEZ <edriennn2001@gmail.com>
2024-08-24 21:32:31 -0400

    implementado el listview de los actores en peliculas individuales

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/config/helpers/number_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/widgets/shared/poster_gradient.dart';
@@ -164,19 +165,71 @@ class _MovieDetails extends StatelessWidget {
 
 
         //Actores
-        
+        _ActorsByMovie(movieID: movie.id.toString()),
 
 
 
 
         const SizedBox(
-          height: 100,
+          height: 50,
         ),
       ],
     );
   }
 }
 
+class _ActorsByMovie extends ConsumerWidget {
+  final String movieID;
+  const _ActorsByMovie({required this.movieID});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final actorByMovie = ref.watch(actorsByMovieProvider);
+
+    if(actorByMovie[movieID] == null) return const CircularProgressIndicator(strokeWidth: 2,);
+
+    final actors = actorByMovie[movieID]!;
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+          return Container(
+            padding: const EdgeInsets.all(8),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Photo actor
+                FadeInRight(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                      ),
+                  ),
+                ),
+                // Nombre de actor
+                Text(actor.name, maxLines: 2,),
+                Text(
+                  '(${actor.character})', 
+                  maxLines: 2,
+                  style: const TextStyle(fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis),
+                  ),
+                
+
+              ],
+              ),
+          );
+        },),
+    );
+  }
+}
 class _CustomSliver extends StatelessWidget {
   final Movie movie;
   const _CustomSliver({required this.movie});
@@ -201,6 +254,10 @@ class _CustomSliver extends StatelessWidget {
               child: Image.network(
                 movie.posterPath,
                 fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if(loadingProgress != null) return const SizedBox();
+                  return FadeIn(child: child);
+                },
               ),
             ),
             const PosterGradient(),
