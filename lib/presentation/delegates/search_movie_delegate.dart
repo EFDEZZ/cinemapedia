3b872b4bdef01e3cced0371fafee24f342dffd3a
EDRIEN FDEZ <edriennn2001@gmail.com>
2024-08-25 15:30:17 -0400

    Creando el delegate para la busqueda, build leading y buildActions implementados

---
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -0,0 +1,45 @@
+
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
+
+class SearchMovieDelegate extends SearchDelegate<Movie?>{
+
+  @override
+  String get searchFieldLabel => 'Buscar Película';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return[
+
+      // if(query.isNotEmpty)
+      FadeIn(
+        animate: query.isNotEmpty,
+        child: IconButton(
+          onPressed: (){
+            query = '';
+        }, icon: const Icon(Icons.clear)),
+      )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: (){
+        close(context, null);
+      }, 
+      icon: const Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text('buildResults');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return const Text('buildSuggestions');
+  }
+
+}
