81bc450458e23966d8bf026905600b96d6a0a815
EDRIEN FDEZ <edriennn2001@gmail.com>
2024-08-27 21:43:53 -0400

    Bug con el overview arreglado

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -31,7 +31,9 @@ class MovieMapper {
       : 'https://res.cloudinary.com/teepublic/image/private/s--UNm73W6m--/t_Preview/b_rgb:191919,c_limit,f_auto,h_630,q_90,w_630/v1606803184/production/designs/16724220_0.jpg',
       originalLanguage: movieDB.originalLanguage,
       originalTitle: movieDB.originalTitle,
-      overview: movieDB.overview,
+      overview: (movieDB.overview != '')
+      ? movieDB.overview
+      : "Sinopsis no disponible",
       posterPath: (movieDB.posterPath != '') 
       ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
       : 'https://res.cloudinary.com/teepublic/image/private/s--UNm73W6m--/t_Preview/b_rgb:191919,c_limit,f_auto,h_630,q_90,w_630/v1606803184/production/designs/16724220_0.jpg',
