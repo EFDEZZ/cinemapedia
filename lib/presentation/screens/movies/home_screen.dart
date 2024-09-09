4a12c00dd34fe776780fdd22176bc78c6ee1afe7
EDRIEN FDEZ <edriennn2001@gmail.com>
2024-09-09 14:16:35 -0400

    Ocultando la barra de navegacion del dispositivo

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,17 +1,30 @@
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   // static const name = 'home-screen';
   final Widget childView;
   const HomeScreen({super.key, required this.childView});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
+                      SystemUiOverlay.top,
+                    ]);
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: const CustomBottomnavigation(),
-      body: childView,
+      body: widget.childView,
     );
   }
 }
