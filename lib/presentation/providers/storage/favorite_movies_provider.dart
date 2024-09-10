e65da174251325a588b195ccc5668bbfe3504656
EDRIEN FDEZ <edriennn2001@gmail.com>
2024-09-09 20:27:58 -0400

    Modificacion del provider para que retorne un mapa de {int: peliculas}

--- lib/presentation/providers/storage/favorite_movies_provider.dart
+++ lib/presentation/providers/storage/favorite_movies_provider.dart
@@ -18,7 +18,7 @@ class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>>{
     required this.localStorageRepository
     }):super({});
 
-  Future<void> loadNextPage() async{
+  Future<List<Movie>> loadNextPage() async{
     final movies = await localStorageRepository.loadMovies(offset: page*10,);
     page++;
 
@@ -27,6 +27,8 @@ class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>>{
       tempMoviesMap[movie.id] = movie;
     }
     state = {...state, ...tempMoviesMap};
+
+    return movies;
   }
 
 }
